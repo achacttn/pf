d3f1933ab5e213e9450d8e01e3fab23b62235af7
Scott Lee <achacttn@gmail.com>
2018-05-21 15:06:35 +1000

    navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,11 @@ class PagesController < ApplicationController
         @query = params[:query]
         search_url = "https://content.guardianapis.com/search?api-key=3586b109-00df-4137-9778-6a629c781425"
         @results = HTTParty.get( search_url )
+
+        @article_array = []
+        Article.find_each do |x|
+            @article_array.push(x)
+        end
     end
 end
 
