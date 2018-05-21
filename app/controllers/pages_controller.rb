244a5c949473b7aacf0e4d351cfba77603870a5c
Scott Lee <achacttn@gmail.com>
2018-05-21 17:40:03 +1000

    asd

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,16 @@
 class PagesController < ApplicationController
+    $API_KEY = ENV["guardian_api_key"]
     def home
-        @API_KEY = ENV["guardian_api_key"]
         @query = params[:query]
-        search_url = "https://content.guardianapis.com/search?api-key=3586b109-00df-4137-9778-6a629c781425"
+        search_url = "https://content.guardianapis.com/search?api-key=#{$API_KEY}"
         @results = HTTParty.get( search_url )
 
         @article_array = []
         Article.find_each do |x|
             @article_array.push(x)
         end
+    end
+    def search
     end
 end
 
