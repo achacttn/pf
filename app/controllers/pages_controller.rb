97ac5fb5883eaaa66f0bb5dbbb3b25e73f774b3d
Scott Lee <achacttn@gmail.com>
2018-05-21 19:09:43 +1000

    search and category functions working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,25 @@
 class PagesController < ApplicationController
     $API_KEY = ENV["guardian_api_key"]
     def home
-        @query = params[:query]
-        search_url = "https://content.guardianapis.com/search?api-key=#{$API_KEY}"
-        @results = HTTParty.get( search_url )
-
         @article_array = []
         Article.find_each do |x|
             @article_array.push(x)
         end
-    end
-    def search
-    end
-end
 
+        general_fetch = "https://content.guardianapis.com/search?api-key=#{$API_KEY}"
+        @results = HTTParty.get( general_fetch )
+    end
+
+    def search
+        @searching = params[:search_query]
+        search_url = "https://content.guardianapis.com/search?q=#{@searching}&api-key=#{$API_KEY}"
+        @search_results = HTTParty.get( search_url )
+    end
+
+    def category
+        @category = params[:category].parameterize
+        category_url = "https://content.guardianapis.com/#{@category}?api-key=#{$API_KEY}"
+        @category_results = HTTParty.get( category_url )
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   
   root to: "pages#home"
 
+  get "/search" => "pages#search"
+
+  get "/:category" => "pages#category"
+
   resources :users
   
 end
